f63baf0e0aeb850184197851a750cc369a3b85c5
husty530 <husty530@gmail.com>
2020-11-04 20:39:16 +0900

    *

--- CpClient/CpClient.cpp
+++ CpClient/CpClient.cpp
@@ -1,10 +1,58 @@
 ﻿// C++のクライアントアプリ
 // サーバークライアント間の約束事はIPおよびポート番号を一致させること、閉じるときの合図を揃えておくことです
 
-#include <iostream>
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <conio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char buffer[1024];
+	int dstSocket;
+	struct sockaddr_in dstAddr;
+	WSADATA data;
+	WSAStartup(MAKEWORD(2, 0), &data);
+
+	// sockaddr_in 構造体のセット
+	memset(&dstAddr, 0, sizeof(dstAddr));
+	dstAddr.sin_port = htons(3000);
+	dstAddr.sin_family = AF_INET;
+	dstAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	// ソケットの生成、ストリームを流す
+	dstSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	//接続
+	if (connect(dstSocket, (struct sockaddr*)&dstAddr, sizeof(dstAddr))) return(-1);
+	printf("connect with 127.0.0.1\n");
+
+	int count = 0;
+	bool loop = true;
+	while (loop) {
+		if (_kbhit())
+		{
+			if (_getch() == 13)
+			{
+				const char* sendmsg = "finish\n";
+				send(dstSocket, sendmsg, 1024, 0);
+				printf("client -> %s\n", sendmsg);
+				loop = false;
+			}
+		}
+		//送信
+		count++;
+		const char* sendmsg = (char*)count;
+		send(dstSocket, sendmsg, 1024, 0);
+		printf("client -> %s\n", sendmsg);
+		//受信
+		recv(dstSocket, buffer, 1024, 0);
+		printf("server -> %s\n", buffer);
+		Sleep(500);
+	}
+
+	closesocket(dstSocket);
+	WSACleanup();
+	return(0);
 }
 
--- CpServer/CpServer.cpp
+++ CpServer/CpServer.cpp
@@ -1,10 +1,63 @@
 ﻿// C++のサーバーアプリ
 // サーバークライアント間の約束事はIPおよびポート番号を一致させること、閉じるときの合図を揃えておくことです
 
-#include <iostream>
+#include <stdio.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// ポート番号，ソケット
+	int srcSocket;  // 自分
+	int dstSocket;  // 相手
+
+	// sockaddr_in 構造体
+	struct sockaddr_in srcAddr;
+	struct sockaddr_in dstAddr;
+	int dstAddrSize = sizeof(dstAddr);
+	int status;
+	int numrcv;
+	char buffer[1024];
+
+	WSADATA data;
+	WSAStartup(MAKEWORD(2, 0), &data);
+
+	// sockaddr_in 構造体のセット
+	memset(&srcAddr, 0, sizeof(srcAddr));
+	srcAddr.sin_port = htons(3000);
+	srcAddr.sin_family = AF_INET;
+	srcAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// ソケットの生成（ストリーム型）
+	srcSocket = socket(AF_INET, SOCK_STREAM, 0);
+	// ソケットのバインド
+	bind(srcSocket, (struct sockaddr*)&srcAddr, sizeof(srcAddr));
+	// 接続の許可
+	listen(srcSocket, 1);
+
+	dstSocket = accept(srcSocket, (struct sockaddr*)&dstAddr, &dstAddrSize);
+	printf("connect %s \n", inet_ntoa(dstAddr.sin_addr));
+
+	int count = 0;
+	bool loop = true;
+	while (loop) {
+		//受信
+		numrcv = recv(dstSocket, buffer, sizeof(char) * 1024, 0);
+		if (numrcv == 0 || numrcv == -1) {
+			loop = false;
+			status = closesocket(dstSocket);
+			break;
+		}
+		printf("client -> %s", buffer);
+		for (int i = 0; i < numrcv; i++) { // bufの中の小文字を大文字に変換
+			buffer[i] = toupper(buffer[i]);
+		}
+		//送信
+		send(dstSocket, buffer, sizeof(char) * 1024, 0);
+		printf("server -> %s \n", buffer);
+	}
+
+	WSACleanup();
+	return(0);
 }
 
